1a342e60531bcf3e275fc50c3aa56cebbf6a9938
janet-dev <>
2023-05-17 13:57:15 +0000

    Render data to template todo_list.html

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
+# access item model
+from .models import Item
+
+
 # Create your views here.
 
 
 def get_todo_list(request):
-    return render(request, 'todo/todo_list.html')
+    # get query set of items in db
+    items = Item.objects.all()
+    # context is variable for dictionary of all items
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
